a29709784e87b3423263764ec5d974c9da3ce309
loobin <loobin@naver.com>
2026-05-28 09:58:25 +0900

    fix : 수어표현검색 수정0528

--- back/app/core/mysql_database.py
+++ back/app/core/mysql_database.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
