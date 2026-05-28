6927e7053e0f73146915b464337dfd1a0284b0ad
lia <lia_test@gamil.com>
2026-05-28 13:12:37 +0900

    feat:퀴즈 백엔드 구현

---
+++ back/app/models/quiz.py
@@ -0,0 +1,19 @@
+QUIZ_COLLECTION = "quizzes"
+QUIZ_RESULT_COLLECTION = "quiz_results"
+
+QUIZ_FIELDS = (
+    "quiz_id",
+    "question",
+    "answer",
+    "quiz_type",
+    "lesson_id",
+    "lesson_id2",
+)
+
+QUIZ_RESULT_FIELDS = (
+    "result_id",
+    "is_correct",
+    "answered_at",
+    "user_id",
+    "quiz_id",
+)
